9fb852522253049764304d69baa57cd611b2c251
Riska Agung <riskaagung11@gmail.com>
2023-01-30 08:50:54 +0700

    menampilkan dan menyembunyikan configuration modal

--- script/config.js
+++ script/config.js
@@ -1,1 +1,9 @@
-function openPlayerConfig() {}
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function cancelPlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
