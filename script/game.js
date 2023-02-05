7265e224d8bdb5cad26e612d9824b3b1b201c4a3
Riska Agung <riskaagung11@gmail.com>
2023-02-05 15:27:05 +0700

    Managing Game Rounds and Field Selection

--- script/game.js
+++ script/game.js
@@ -3,5 +3,22 @@ function starNewGame() {
     alert("please sert custom player names for both players!");
     return;
   }
+  activePlayerGameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
+}
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerGameElement.textContent = players[activePlayer].name;
+}
+function selectGameField(event) {
+  if (event.target.tagName !== "LI") {
+    return;
+  }
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disable");
+  switchPlayer();
 }
