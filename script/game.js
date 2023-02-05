ba529e5e0ab575379a4d5c7d0bce7cf8f0011e84
Riska Agung <riskaagung11@gmail.com>
2023-02-05 21:06:31 +0700

    Ending The Game & Adding Restart Logic

--- script/game.js
+++ script/game.js
@@ -1,8 +1,29 @@
+function resetNewgameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML = 'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = "";
+      gameBoardItemElement.classList.remove("disable");
+      gameBoardIndex++;
+    }
+  }
+}
 function starNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("please sert custom player names for both players!");
     return;
   }
+
+  resetNewgameStatus();
+
   activePlayerGameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
 }
@@ -15,7 +36,7 @@ function switchPlayer() {
   activePlayerGameElement.textContent = players[activePlayer].name;
 }
 function selectGameField(event) {
-  if (event.target.tagName !== "LI") {
+  if (event.target.tagName !== "LI" || gameIsOver) {
     return;
   }
   const selectedField = event.target;
@@ -33,7 +54,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
@@ -63,4 +87,15 @@ function checkForGameOver() {
     return -1;
   }
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw";
+  }
 }
